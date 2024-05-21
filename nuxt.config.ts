138a189452a60f29b671ebe0e4db968209b75b24
Cleyson <cleysonph@gmail.com>
2024-05-21 04:20:12 -0300

    setup do eslint e prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,26 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import path from 'node:path'
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts", "nuxt-primevue"],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', 'nuxt-primevue'],
 
-  css: ["primeicons/primeicons.css"],
+  css: ['primeicons/primeicons.css'],
 
   googleFonts: {
     base64: true,
-    fontsDir: "assets/fonts",
+    fontsDir: 'assets/fonts',
     overwriting: true,
     families: {
       Inter: [300, 500, 800],
     },
   },
-});
+
+  primevue: {
+    options: { unstyled: true },
+    importPT: {
+      as: 'lara',
+      from: path.resolve(__dirname, './assets/presets/lara/'),
+    },
+  },
+})
