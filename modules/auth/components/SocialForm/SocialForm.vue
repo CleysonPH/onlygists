da1de4f7ebe5b3d6c9c78210669a60e773722076
Cleyson <cleysonph@gmail.com>
2024-05-21 11:19:13 -0300

    Tela de login com <SocialForm /> e novo módulo de auth

---
+++ modules/auth/components/SocialForm/SocialForm.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const emit = defineEmits<{
+  (e: 'wants-login-with-github'): void
+}>()
+
+const props = defineProps<{
+  loading: boolean
+}>()
+</script>
+
+<template>
+  <div
+    class="flex flex-col items-center justify-center gap-2 rounded bg-gray-50 p-6"
+  >
+    <h2 class="font=[Inter] font-regular text-gray-700">
+      Entre com a sua conta
+    </h2>
+    <Button
+      @click="() => emit('wants-login-with-github')"
+      :loading="props.loading"
+      label="Continuar com o GitHub"
+      icon="pi pi-github"
+      icon-pos="right"
+    ></Button>
+  </div>
+</template>
---
+++ modules/auth/screens/Login/Login.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import SocialForm from '@/modules/auth/components/SocialForm/SocialForm.vue'
+
+const loading = ref<boolean>(false)
+
+const handleGithubLogin = () => {
+  loading.value = true
+  // TODO: Call the login service
+}
+</script>
+
+<template>
+  <section
+    class="flex h-screen w-full flex-col items-center justify-center gap-5 bg-gray-50"
+  >
+    <Logo />
+    <SocialForm
+      :loading="loading"
+      @wants-login-with-github="handleGithubLogin"
+    />
+  </section>
+</template>
