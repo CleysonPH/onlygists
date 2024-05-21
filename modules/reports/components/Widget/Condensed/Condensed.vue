6a3274132987fe9a256f44b2a746d1858f367948
Cleyson <cleysonph@gmail.com>
2024-05-21 11:51:29 -0300

    Novo módulo de reports e criação do <WidgetGroup /> e <WidgetCondensed />

---
+++ modules/reports/components/Widget/Condensed/Condensed.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{
+  value: number | string
+  label: string
+}>()
+</script>
+
+<template>
+  <div class="flex flex-1 flex-col rounded-xl bg-white p-5">
+    <span class="font-[Inter] text-4xl font-bold text-primary-500">{{
+      props.value
+    }}</span>
+    <p class="font-[Inter] text-lg text-gray-700">{{ props.label }}</p>
+  </div>
+</template>
--- modules/users/screens/PublicProfile/PublicProfile.vue
+++ modules/users/screens/PublicProfile/PublicProfile.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import PublicHeadline from '@/modules/users/components/PublicHeadline/PublicHeadline.vue'
+import WidgetGroup from '@/modules/reports/components/Widget/Group/Group.vue'
+import WidgetCondensed from '@/modules/reports/components/Widget/Condensed/Condensed.vue'
 </script>
 
 <template>
@@ -10,4 +12,10 @@ import PublicHeadline from '@/modules/users/components/PublicHeadline/PublicHead
     city="Teresina"
     satate="PI"
   />
+
+  <WidgetGroup>
+    <WidgetCondensed :value="10" label="Gists no total" />
+    <WidgetCondensed :value="3" label="Gists gratuitos" />
+    <WidgetCondensed :value="7" label="Gists pagos" />
+  </WidgetGroup>
 </template>
